e441ce064922c5311d8c13c3942572a98170c52e
uhziel <uhziel@gmail.com>
2022-04-03 07:23:32 +0800

    添加unity测试项目 example-2018.4.29f1

---
+++ example-2018.4.29f1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    public Vector3 camPosOffset;
+    public Vector3 camRotOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = plane.transform.rotation * CalRotOffset(); //Sets the rotation to the plane rot + the offset given at the beginning
+        transform.position = plane.transform.position + CalPosOffset(); //Sets the position to the plane pos + (the current rotation of the cam * the offset given at the beginning) //In that order
+    }
+    private Quaternion CalRotOffset()
+    {
+        return Quaternion.Euler(camRotOffset);
+    }
+
+    private Vector3 CalPosOffset()
+    {
+        return transform.rotation * camPosOffset;
+        //By far the most interesting par of the challange that wasn't even a challange
+        //rotate the camara based on the rotation of the plane and make it stay there even if the plane keeps rotating
+    }
+
+}
---
+++ example-2018.4.29f1/README.md
@@ -0,0 +1,3 @@
+# example-2018.4.29f1
+
+via https://github.com/Jeffsmov/UnityTutorials/tree/main/Plane%20Challenge
